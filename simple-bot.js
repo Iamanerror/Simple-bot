ff41fd0b8dbf9639f1c869b14c48e5d929df1e86
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2018-10-07 15:39:18 +0530

    Update simple-bot.js

--- simple-bot.js
+++ simple-bot.js
@@ -76,13 +76,27 @@ bot.on('guildMemberRemove', member => {
 
 });
 
-bot.on('avatar', member => {
+bot.on('message', message =>{
 
-     if (message.content === 'what is my avatar') {
+    // Variables
+
+    var sender = message.author; //The person who sent the message
+
+    var msg = message.content;
+
+    var prefix = "." //The text before commands, you can put anything that you prefer
+
+    if(message.author.id != "498152471278125076" && message.channel.id === "496740651065540610"){
+
+        if(msg.startsWith('.', 0)){
+
+            if(msg === prefix + "henlo" && message.channel.id === "496740651065540610"){
+
+                message.channel.send('murgilo') // Sends a message to the channel, with the content
          
-        message.reply(message.author.displayAvatarURL());
-
 });
+
+
 
 
 
